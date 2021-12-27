b09de53544c35ad834decb2c1fd963ef48ea49dc
honehone <honehone.41games@gmail.com>
2021-12-27 16:22:16 +0900

    commented out  loginfo.

--- src/serial_end_point.cpp
+++ src/serial_end_point.cpp
@@ -86,7 +86,7 @@ public:
         signedAngleX = msg->value.x > 180.0f ? msg->value.x - 360.0f : msg->value.x;
         signedAngleY = msg->value.y > 180.0f ? msg->value.y - 360.0f : msg->value.y;
 
-        ROS_INFO("angle x %f y %f", msg->value.x, msg->value.y);
+        //ROS_INFO("angle x %f y %f", msg->value.x, msg->value.y);
     }
 };
 
